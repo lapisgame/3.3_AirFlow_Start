8045935d4f51f1bb61bc79172a7dbb623e1d2222
lapis <klimantovartem@gmail.com>
2023-08-25 23:06:37 +0300

    Dag

--- task_3.3.py
+++ task_3.3.py
@@ -5,175 +5,109 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 
-from urllib.parse import urlencode
-
 import psycopg2
 import requests
 import datetime
-import calendar
-
 
 # database credentials
-host = 'localhost'
-port = '5430'
-user = 'postgres'
-password = 'password'
-database = 'test'
+db_name = 'test'
+db_user = 'postgres'
+db_pass = 'password'
+db_host = 'db'
+db_port = '5432'
 
-headers = ['currency', 'basic_curr', 'rate_date', 'rate'] 
-mart_headers = ["валюта", "день_макс", "день_мин", "курс_макс", "курс_мин", "курс_сред", "курс_посл"]
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+
+# rates table
+sql_string = '''
+            create table if not exists rates
+            (
+	            ticker varchar(32),
+                rate_date varchar(32),
+	            rate varchar(32)
+            );'''
 
 
 # вспомогательные функции
-def create_sql(ticker):
-    return f'''create table if not exists ticker_{ticker} (
-        row_id serial,
-        rate_date varchar(32),
-        currency varchar(32),
-        basic_curr varchar(32),
-        rate varchar(32)
-);'''
-
-
-def data_mart_string(ticker):
-	return f'''
-	
-	with tmp_maxx ("валюта", "день_макс", "курс_макс") as (
-		select distinct currency, rate_date, rate 
-		from ticker_{ticker}
-		where rate = (select max(rate) from ticker_{ticker})
-		limit 1
-	),
-
-	tmp_minn ("день_мин", "курс_мин") as (
-		select distinct rate_date, rate
-		from ticker_{ticker}
-		where rate = (select min(rate) from ticker_{ticker})
-		limit 1
-	),
-
-	tmp_aver ("средний курс") as (select avg(rate::numeric)::varchar from ticker_{ticker}),
-
-	tmp_lday ("последний курс") as (
-		select rate
-		from ticker_{ticker}
-		order by rate_date desc
-		limit 1
-	)
-
-	select * from tmp_maxx, tmp_minn, tmp_aver, tmp_lday;
-'''
-
-def main():
-    data_mart_table = '''
-	create table if not exists curr_report (
-            "валюта" varchar(32), 
-            "день_макс" varchar(32),
-            "курс_макс" varchar(32),
-            "день_мин" varchar(32),
-            "курс_мин" varchar(32),
-            "курс_сред" varchar(32),
-            "курс_посл" varchar(32)
-	);
-    '''
-    
-
-    symbols = ['RUB', 'USD', 'EUR', 'GBP', 'AED', 'AFN']
-    base = 'BTC'
-
-    now = datetime.datetime.now()
-
-    _, eend = calendar.monthrange(now.year, now.month)
-    mon = f'0{now.month}' if now.month < 10 else now.month
-
-
-    base_url = 'https://api.exchangerate.host/timeseries?'
-    params = { 
-        'start_date':f'{now.year}-{mon}-01',
-        'end_date':f'{now.year}-{mon}-{eend}',
-        'symbols':','.join(symbols),
-        'base': base   
-    }
-
-    response = requests.get(base_url + urlencode(params))
-    data = response.json()['rates']
-
-    _ = []
-    for k, v in data.items():
-        for k1 in v.keys():        
-            _.append((k1, base, k, v[k1]))
-            
-
-    for symb in symbols:
-        data = [line for line in _ if line[0] == symb]
-        
-        create = create_sql(symb)
-        data_mart = data_mart_string(symb)
-    
-        
-        with psycopg2.connect(host=host, port=port, user=user, password=password, database=database) as conn:
-            cursor = conn.cursor()
-            cursor.execute(create)
-            conn.commit()
-
-            for row in data:
-                cursor.execute(f"insert into ticker_{symb} ({', '.join(headers)}) values ({', '.join(['%s'] * len(headers))})", row)
-                conn.commit()     
-            print(f'insert into ticker_{symb} OK')  
-        conn.close()
-
-    report_data = list()
-
-    for symb in symbols:
-        with psycopg2.connect(host=host, port=port, user=user, password=password, database=database) as conn:
-            data_mart = data_mart_string(symb)
-            
-            cursor = conn.cursor()
-            cursor.execute(data_mart)
-            report_data.append(cursor.fetchone())
-            conn.commit()
-        conn.close()
-
-    with psycopg2.connect(host=host, port=port, user=user, password=password, database=database) as conn:
+def create_table():
+    with psycopg2.connect(db_string) as conn:
         cursor = conn.cursor()
-        cursor.execute(data_mart_table)
+        cursor.execute(sql_string)
         conn.commit()
-                
-        for row in report_data:
-            cursor.execute(f"INSERT INTO curr_report VALUES ({', '.join(['%s'] * len(mart_headers))})", row)
+    conn.close()   
 
-    conn.commit()
+def rate_get():
+    url = 'https://api.exchangerate.host/latest?'
+    response = requests.get(url)
+    data = response.json()['rates']
+    rate = data['RUB'] / data['BTC']
 
+    dt = datetime.datetime.now()
     
+    return ('BTC', dt.strftime('%H:%M - %m.%d.%Y'), rate)
+
+def data_insert(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='get_rate')
+
+    with psycopg2.connect(db_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+                f"INSERT INTO rates ({', '.join(['ticker', 'rate_date', 'rate'])}) VALUES ({', '.join(['%s'] * 3)})",
+                data
+            )
+        conn.commit()
+    conn.close()   
+
+
 #Объявление DAG
 default_args = {
     'owner': 'airflow',    
     'start_date': airflow.utils.dates.days_ago(2),
+    # 'end_date': datetime(),
     'depends_on_past': False,
+    #'email': ['airflow@example.com'],
+    #'email_on_failure': False,
+    #'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-task_33 = DAG(
-    dag_id = 'task_33',
+task_33_new = DAG(
+    dag_id = 'task_33_new',
     default_args=default_args,
     schedule_interval='*/10 * * * *',
+    #schedule_interval='@daily',	
     dagrun_timeout=timedelta(minutes=60),
     description='task 3.2 decision',
     start_date = airflow.utils.dates.days_ago(1)
 )
 
-Good_morning = BashOperator(
-    task_id="Good_morning",
+print_line = BashOperator(
+    task_id="print_line",
     bash_command='echo Good morning my diggers!',
-    dag=task_33
+    dag=task_33_new
 )
 
-main_task = PythonOperator(
-    task_id='main_task',
-    python_callable=main,
+create_task = PythonOperator(
+    task_id='create_task',
+    python_callable=create_table,
     provide_context=True,
-    dag = task_33
-)
+    dag = task_33_new
+    )
 
-Good_morning >> main_task
+get_rate = PythonOperator(
+    task_id='get_rate',
+    python_callable=rate_get,
+    provide_context=True,
+    dag = task_33_new
+    )
+
+insert_data = PythonOperator(
+    task_id='insert_data',
+    python_callable=data_insert,
+    provide_context=True,
+    dag = task_33_new
+    )
+
+print_line >> create_task >> get_rate >> insert_data
